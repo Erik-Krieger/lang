541902e262bce0c82908fcdd96d4a19663434d3d
Erik Krieger <30746242+Erik-Krieger@users.noreply.github.com>
2026-01-05 21:16:44 +0100

    Tokenization works

--- lexer.go
+++ lexer.go
@@ -7,11 +7,66 @@ import (
 
 type TokenType uint64
 
+func (t TokenType) String() string {
+	switch t {
+	case TOKEN_TYPE_NONE:
+		return "TOKEN_TYPE_NONE"
+	case TOKEN_TYPE_NUMBER:
+		return "TOKEN_TYPE_NUMBER"
+	case TOKEN_TYPE_NUMBER_INT:
+		return "TOKEN_TYPE_NUMBER_INT"
+	case TOKEN_TYPE_NUMBER_FLOAT:
+		return "TOKEN_TYPE_NUMBER_FLOAT"
+	case TOKEN_TYPE_IDENTIFIER:
+		return "TOKEN_TYPE_IDENTIFIER"
+	case TOKEN_TYPE_OPERATOR:
+		return "TOKEN_TYPE_OPERATOR"
+	case TOKEN_TYPE_KEYWORD:
+		return "TOKEN_TYPE_KEYWORD"
+	case TOKEN_TYPE_DATA_TYPE:
+		return "TOKEN_TYPE_DATA_TYPE"
+	case TOKEN_TYPE_SEMICOLON:
+		return "TOKEN_TYPE_SEMICOLON"
+	default:
+		return "Error: Unknown Type"
+	}
+}
+
 const (
-	TOKEN_TYPE_NONE       TokenType = (1 << 0)
-	TOKEN_TYPE_NUMBER     TokenType = (1 << 1)
-	TOKEN_TYPE_IDENTIFIER TokenType = (1 << 2)
-	TOKEN_TYPE_DASH       TokenType = (1 << 3)
+	TOKEN_TYPE_NONE                                TokenType = 0
+	TOKEN_TYPE_NUMBER                              TokenType = 100
+	TOKEN_TYPE_NUMBER_INT                          TokenType = 101
+	TOKEN_TYPE_NUMBER_FLOAT                        TokenType = 102
+	TOKEN_TYPE_IDENTIFIER                          TokenType = 200
+	TOKEN_TYPE_KEYWORD                             TokenType = 300
+	TOKEN_TYPE_OPERATOR                            TokenType = 400
+	TOKEN_TYPE_OPERATOR_UNARY                      TokenType = 401
+	TOKEN_TYPE_OPERATOR_LOGICAL_OR                 TokenType = 410
+	TOKEN_TYPE_OPERATOR_LOGICAL_AND                TokenType = 411
+	TOKEN_TYPE_OPERATOR_LOGICAL_NOT                TokenType = 412
+	TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_LEFT         TokenType = 450
+	TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_LEFT_ASSIGN  TokenType = 451
+	TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_RIGHT        TokenType = 452
+	TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_RIGHT_ASSIGN TokenType = 453
+	TOKEN_TYPE_OPERATOR_BITWISE_OR                 TokenType = 460
+	TOKEN_TYPE_OPERATOR_BITWISE_OR_ASSIGN          TokenType = 461
+	TOKEN_TYPE_OPERATOR_BITWISE_AND                TokenType = 462
+	TOKEN_TYPE_OPERATOR_BITWISE_AND_ASSIGN         TokenType = 463
+	TOKEN_TYPE_OPERATOR_BITWISE_XOR                TokenType = 464
+	TOKEN_TYPE_OPERATOR_BITWISE_XOR_ASSIGN         TokenType = 464
+	TOKEN_TYPE_DATA_TYPE                           TokenType = 500
+	TOKEN_TYPE_SEMICOLON                           TokenType = 600
+	TOKEN_TYPE_COLON                               TokenType = 601
+	TOKEN_TYPE_COMMA                               TokenType = 602
+	TOKEN_TYPE_PERIOD                              TokenType = 603
+	TOKEN_TYPE_OPEN_PAREN                          TokenType = 1000
+	TOKEN_TYPE_CLOSE_PAREN                         TokenType = 1001
+	TOKEN_TYPE_OPEN_BRACKET                        TokenType = 1100
+	TOKEN_TYPE_CLOSE_BRACKET                       TokenType = 1101
+	TOKEN_TYPE_OPEN_CURLY                          TokenType = 1200
+	TOKEN_TYPE_CLOSE_CURLY                         TokenType = 1201
+	TOKEN_TYPE_OPEN_ANGLE                          TokenType = 1300
+	TOKEN_TYPE_CLOSE_ANGLE                         TokenType = 1301
 )
 
 type lexerContainer struct {
@@ -105,7 +160,7 @@ func isBracket(c rune) bool {
 }
 
 func isSpecialCharacter(c rune) bool {
-	return isOperator(c) || c == '=' || c == '!' || c == ';' || c == ':' || c == ',' || c == '.'
+	return isOperator(c) || c == '=' || c == '!' || c == ';' || c == ':' || c == ',' || c == '.' || c == '^' || c == '&' || c == '|' || c == '~'
 }
 
 func isOperator(c rune) bool {
--- tokenizer.go
+++ tokenizer.go
@@ -1,8 +1,411 @@
 package main
 
-func tokenize(lexerTokens []string) error {
-	for idx, token := range lexerTokens {
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
 
+type Token struct {
+	Type  TokenType
+	Value string
+}
+
+var keywordMap map[string]Token = map[string]Token{
+	"pub": {
+		Type:  TOKEN_TYPE_KEYWORD,
+		Value: "pub",
+	},
+	"fn": {
+		Type:  TOKEN_TYPE_KEYWORD,
+		Value: "fn",
+	},
+	"return": {
+		Type:  TOKEN_TYPE_KEYWORD,
+		Value: "return",
+	},
+	"let": {
+		Type:  TOKEN_TYPE_KEYWORD,
+		Value: "let",
+	},
+	"mut": {
+		Type:  TOKEN_TYPE_KEYWORD,
+		Value: "mut",
+	},
+	"u32": {
+		Type:  TOKEN_TYPE_DATA_TYPE,
+		Value: "u32",
+	},
+	"i64": {
+		Type:  TOKEN_TYPE_DATA_TYPE,
+		Value: "i64",
+	},
+}
+var tokens []Token = make([]Token, 0)
+
+func tokenize(lexerTokens []string) error {
+	skipIterationCount := 0
+
+	for idx, token := range lexerTokens {
+		if skipIterationCount > 0 {
+			skipIterationCount -= 1
+			continue
+		}
+
+		switch token {
+		case ";":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_SEMICOLON,
+				Value: ";",
+			})
+			continue
+		case ":":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_COLON,
+				Value: ":",
+			})
+			continue
+		case ",":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_COMMA,
+				Value: ",",
+			})
+			continue
+		case ".":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_COMMA,
+				Value: ".",
+			})
+			continue
+		case "(":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPEN_PAREN,
+				Value: "(",
+			})
+			continue
+		case ")":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_CLOSE_PAREN,
+				Value: ")",
+			})
+			continue
+		case "{":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPEN_CURLY,
+				Value: "{",
+			})
+			continue
+		case "}":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_CLOSE_CURLY,
+				Value: "}",
+			})
+			continue
+		case "[":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPEN_BRACKET,
+				Value: "[",
+			})
+			continue
+		case "]":
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_CLOSE_BRACKET,
+				Value: "]",
+			})
+			continue
+		case "-":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == ">" || lexerTokens[idx+1] == "-" || lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: fmt.Sprintf("-%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				} else if isNumber(rune(lexerTokens[idx+1][0])) {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_NUMBER,
+						Value: fmt.Sprintf("-%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "-",
+			})
+			continue
+		case "+":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "+" || lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: fmt.Sprintf("+%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "+",
+			})
+			continue
+		case "*":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "*" || lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: fmt.Sprintf("*%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "*",
+			})
+			continue
+		case "/":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "/" || lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: fmt.Sprintf("/%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "/",
+			})
+			continue
+		case "%":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: "%=",
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "%",
+			})
+			continue
+		case "=":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: "==",
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "=",
+			})
+			continue
+		case "!":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: "!=",
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "!",
+			})
+			continue
+		case "&":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "&" || lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: fmt.Sprintf("&%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "&",
+			})
+			continue
+		case "|":
+			if len(lexerTokens)-1 > idx {
+				if lexerTokens[idx+1] == "|" || lexerTokens[idx+1] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR,
+						Value: fmt.Sprintf("|%s", lexerTokens[idx+1]),
+					})
+					skipIterationCount = 1
+					continue
+				}
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR,
+				Value: "|",
+			})
+			continue
+		case "^":
+			if len(lexerTokens)-1 > idx && lexerTokens[idx+1] == "=" {
+				tokens = append(tokens, Token{
+					Type:  TOKEN_TYPE_OPERATOR_BITWISE_XOR_ASSIGN,
+					Value: "^=",
+				})
+				skipIterationCount = 1
+				continue
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPERATOR_BITWISE_XOR,
+				Value: "^",
+			})
+			continue
+		case "<":
+			if len(lexerTokens)-1 > idx && lexerTokens[idx+1] == "<" {
+				if len(lexerTokens)-2 > idx && lexerTokens[idx+2] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_LEFT_ASSIGN,
+						Value: "<<=",
+					})
+					skipIterationCount = 2
+					continue
+				}
+
+				tokens = append(tokens, Token{
+					Type:  TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_LEFT,
+					Value: "<<",
+				})
+
+				skipIterationCount = 1
+				continue
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_OPEN_ANGLE,
+				Value: "<",
+			})
+			continue
+		case ">":
+			if len(lexerTokens)-1 > idx && lexerTokens[idx+1] == ">" {
+				if len(lexerTokens)-2 > idx && lexerTokens[idx+2] == "=" {
+					tokens = append(tokens, Token{
+						Type:  TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_RIGHT_ASSIGN,
+						Value: ">>=",
+					})
+					skipIterationCount = 2
+					continue
+				}
+
+				tokens = append(tokens, Token{
+					Type:  TOKEN_TYPE_OPERATOR_BITWISE_SHIFT_RIGHT,
+					Value: ">>",
+				})
+
+				skipIterationCount = 1
+				continue
+			}
+
+			tokens = append(tokens, Token{
+				Type:  TOKEN_TYPE_CLOSE_ANGLE,
+				Value: ">",
+			})
+			continue
+		}
+
+		value, hasKey := keywordMap[token]
+		if hasKey {
+			tokens = append(tokens, value)
+			continue
+		}
+
+		if isNumber(rune(token[0])) {
+			allNum := true
+
+			for _, c := range token[1:] {
+				if isNumber(c) || c == '_' {
+					continue
+				}
+
+				allNum = false
+				break
+			}
+
+			if allNum {
+				var tokenType TokenType
+				numPeriodCount := strings.Count(token, ".")
+				switch numPeriodCount {
+				case 0:
+					tokenType = TOKEN_TYPE_NUMBER_INT
+				case 1:
+					tokenType = TOKEN_TYPE_NUMBER_FLOAT
+				default:
+					return errors.New("Malformed number token.")
+				}
+
+				cleanNum := strings.ReplaceAll(token, "_", "")
+				tokens = append(tokens, Token{
+					Type:  tokenType,
+					Value: cleanNum,
+				})
+				continue
+			}
+		}
+
+		if isLetter(rune(token[0])) || token[0] == '_' {
+			allIdent := true
+			for _, c := range token {
+				if isValidIdentifier(c) {
+					continue
+				}
+
+				allIdent = false
+				break
+			}
+
+			if allIdent {
+				tokens = append(tokens, Token{
+					Type:  TOKEN_TYPE_IDENTIFIER,
+					Value: token,
+				})
+				continue
+			}
+		}
+
+		fmt.Printf("Reached the of the parsing loop with an unhandled token: %s\n", token)
+		os.Exit(1)
+	}
+
+	for idx, tok := range tokens {
+		fmt.Printf("%d: %s, %s\n", idx, tok.Type.String(), tok.Value)
 	}
 
 	return nil
