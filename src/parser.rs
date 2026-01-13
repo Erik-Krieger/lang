f1888e159d7ec85b9a2f927f4a41f7bfdecbe7d0
Erik Krieger <30746242+Erik-Krieger@users.noreply.github.com>
2026-01-13 19:34:36 +0100

    Now parsing directly to tokens

--- src/parser.rs
+++ src/parser.rs
@@ -1,18 +1,48 @@
+use std::collections::HashSet;
 use std::{fs, process};
 
-enum ParserTokenType {
-    None,
-    Identifier,
-    Number,
+#[derive(Debug)]
+pub enum Token {
+    Bogus,
+    OpenParen,
+    CloseParen,
+    OpenCurly,
+    CloseCurly,
+    OpenBracket,
+    CloseBracket,
+    OpenAngle,
+    CloseAngle,
+    ExclamationMark,
+    QuestionMark,
+    Equals,
+    Colon,
+    SemiColon,
+    Period,
+    Comma,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
+    Percent,
+    Underscore,
+    SingleQuote,
+    DoubleQuote,
+    Identifier { name: String },
+    Keyword { name: String },
+    CharLiteral { value: String },
+    StringLiteral { value: String },
+    NumericLiteral { value: String },
 }
 
 struct ParserData {
-    tokens: Vec<String>,
+    tokens: Vec<Token>,
     current_buffer: Vec<char>,
-    current_token: ParserTokenType,
+    current_token: Token,
+    period_count: u64,
+    next_char_escaped: bool,
 }
 
-pub fn parse_file(file_path: &String) -> Vec<String> {
+pub fn parse_file(file_path: &String) -> Vec<Token> {
     let file_content = match fs::read_to_string(file_path) {
         Ok(data) => data,
         Err(err) => {
@@ -21,10 +51,18 @@ pub fn parse_file(file_path: &String) -> Vec<String> {
         }
     };
 
+    let mut keywords = HashSet::new();
+    keywords.insert("fn");
+    keywords.insert("pub");
+    keywords.insert("mut");
+    keywords.insert("return");
+
     let mut parser = ParserData {
         tokens: Vec::new(),
         current_buffer: Vec::new(),
-        current_token: ParserTokenType::None,
+        current_token: Token::Bogus,
+        period_count: 0,
+        next_char_escaped: false,
     };
 
     for token in file_content.chars() {
@@ -36,89 +74,143 @@ pub fn parse_file(file_path: &String) -> Vec<String> {
 
 fn process_token(token: char, parser: &mut ParserData) {
     match parser.current_token {
-        ParserTokenType::None => {
+        Token::Bogus => {
             if token.is_ascii_digit() {
-                parser.current_token = ParserTokenType::Number;
+                parser.current_token = Token::NumericLiteral {
+                    value: String::new(),
+                };
                 parser.current_buffer.push(token);
             } else if token.is_ascii_alphabetic() || token == '_' {
-                parser.current_token = ParserTokenType::Identifier;
+                parser.current_token = Token::Identifier {
+                    name: String::new(),
+                };
                 parser.current_buffer.push(token);
-            } else if is_bracket(token) {
-                parser.tokens.push(token.into());
-            } else if is_special_char(token) {
-                parser.tokens.push(token.into());
+            } else if token == '"' {
+                parser.current_token = Token::StringLiteral {
+                    value: String::new(),
+                };
+            } else if token == '\'' {
+                parser.current_token = Token::CharLiteral {
+                    value: String::new(),
+                };
+            } else {
+                let p_token: Token = match token {
+                    '(' => Token::OpenParen,
+                    ')' => Token::CloseParen,
+                    '{' => Token::OpenCurly,
+                    '}' => Token::CloseCurly,
+                    '[' => Token::OpenBracket,
+                    ']' => Token::CloseBracket,
+                    '<' => Token::OpenAngle,
+                    '>' => Token::CloseAngle,
+                    '!' => Token::ExclamationMark,
+                    '?' => Token::QuestionMark,
+                    '=' => Token::Equals,
+                    ':' => Token::Colon,
+                    ';' => Token::SemiColon,
+                    '.' => Token::Period,
+                    ',' => Token::Comma,
+                    '+' => Token::Plus,
+                    '-' => Token::Minus,
+                    '*' => Token::Asterisk,
+                    '/' => Token::Slash,
+                    '%' => Token::Percent,
+                    '_' => Token::Underscore,
+                    _ => Token::Bogus,
+                };
+
+                match p_token {
+                    Token::Bogus => {}
+                    _ => parser.tokens.push(p_token),
+                }
             }
         }
-        ParserTokenType::Number => {
-            if token.is_ascii_digit() || token == '_' {
+        Token::NumericLiteral { .. } => {
+            if token.is_ascii_digit() || token == '_' || token == ',' {
+                return;
+            } else if token == '.' && parser.period_count == 0 {
+                parser.period_count += 1;
                 parser.current_buffer.push(token);
+            } else if token == '.' && parser.period_count > 0 {
+                panic!();
             } else {
-                parser
-                    .tokens
-                    .push(parser.current_buffer.iter().cloned().collect());
+                let value: String = parser.current_buffer.iter().cloned().collect();
+                let new_token: Token = Token::NumericLiteral { value: value };
                 parser.current_buffer.clear();
-                parser.current_token = ParserTokenType::None;
+                parser.current_token = Token::Bogus;
+                parser.tokens.push(new_token);
 
                 process_token(token, parser);
             }
         }
-        ParserTokenType::Identifier => {
+        Token::Identifier { .. } => {
             if token.is_ascii_alphabetic() || token.is_ascii_digit() || token == '_' {
                 parser.current_buffer.push(token);
             } else {
-                parser
-                    .tokens
-                    .push(parser.current_buffer.iter().cloned().collect());
+                let value: String = parser.current_buffer.iter().cloned().collect();
+
+                if is_keyword(&value) {
+                    parser.tokens.push(Token::Keyword { name: value });
+                } else {
+                    parser.tokens.push(Token::Identifier { name: value });
+                }
+
                 parser.current_buffer.clear();
-                parser.current_token = ParserTokenType::None;
+                parser.current_token = Token::Bogus;
 
                 process_token(token, parser);
             }
         }
+        Token::StringLiteral { .. } => {
+            if !parser.next_char_escaped {
+                if token == '\\' {
+                    parser.next_char_escaped = true;
+                    return;
+                } else if token == '"' {
+                    let value: String = parser.current_buffer.iter().cloned().collect();
+                    let new_token: Token = Token::StringLiteral { value: value };
+                    parser.current_buffer.clear();
+                    parser.current_token = Token::Bogus;
+                    parser.tokens.push(new_token);
+                    return;
+                }
+            } else {
+                parser.next_char_escaped = false;
+            }
+
+            parser.current_buffer.push(token);
+        }
+        Token::CharLiteral { .. } => {
+            if !parser.next_char_escaped {
+                if token == '\\' {
+                    parser.next_char_escaped = true;
+                    return;
+                } else if token == '\'' {
+                    assert!(parser.current_buffer.iter().len() == 0);
+
+                    let value: String = parser.current_buffer.iter().cloned().collect();
+                    let new_token: Token = Token::StringLiteral { value: value };
+                    parser.current_buffer.clear();
+                    parser.current_token = Token::Bogus;
+                    parser.tokens.push(new_token);
+                    return;
+                }
+            } else {
+                parser.next_char_escaped = false;
+            }
+
+            parser.current_buffer.push(token);
+        }
+        _ => unreachable!(),
     };
 }
 
-fn is_bracket(token: char) -> bool {
-    return match token {
-        '(' => true,
-        ')' => true,
-        '{' => true,
-        '}' => true,
-        '[' => true,
-        ']' => true,
-        '<' => true,
-        '>' => true,
+fn is_keyword(token: &String) -> bool {
+    match &token[..] {
+        "fn" => true,
+        "pub" => true,
+        "mut" => true,
+        "return" => true,
         _ => false,
-    };
-}
-
-fn is_operator(token: char) -> bool {
-    return match token {
-        '+' => true,
-        '-' => true,
-        '*' => true,
-        '/' => true,
-        '%' => true,
-        _ => false,
-    };
-}
-
-fn is_special_char(token: char) -> bool {
-    if is_operator(token) {
-        return true;
     }
-
-    return match token {
-        '=' => true,
-        '!' => true,
-        ';' => true,
-        ':' => true,
-        ',' => true,
-        '.' => true,
-        '^' => true,
-        '&' => true,
-        '|' => true,
-        '~' => true,
-        _ => false,
-    };
 }
